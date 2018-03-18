d1b5a5ebf2f61d1972461a4d8594a7de5de78300
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-03-18 23:12:30 +0100

    first commit

---
+++ asks_for_name/asks_for_name.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, what\'s your first name?'
+name1 = gets.chomp' '
+
+puts 'Thats a nice name! Now what\'s your middle name?'
+name2 = gets.chomp' '
+
+puts 'Amazing! Finally, what\'s your lastname?'
+name3 = gets.chomp
+
+puts 'Magnifical! Please to meet you '
++ name1 + name2 + name3 + '? :)'
