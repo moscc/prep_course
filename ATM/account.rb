d10926c2d4235708815a16a5bbe25f3170ecb017
mathias_oscarsson <mathiasoscarsson@gmail.com>
2018-04-04 00:07:40 +0200

    commit

---
+++ ATM/account.rb
@@ -0,0 +1,36 @@
+require 'date'
+
+class Account
+  STANDARD_VALIDITY_YRS = 5
+  attr_reader :exp_date, :owner, :pin_code
+  attr_accessor :balance, :status
+
+  def initialize(attrs = {})
+    @exp_date = set_expiry_date
+    @status = :active
+    @balance = 0
+    @pin_code = '1234'
+    set_owner(attrs[:owner])
+  end
+
+  def set_expiry_date
+    Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+  end
+
+  def self.deactivate(account)
+    account.account_status = :deactivated
+  end
+
+  def deactivate
+    @status = :deactivated
+  end
+
+  def missing_owner
+    raise "An account owner is required"
+  end
+
+  private
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+end
---
+++ ATM/spec/account_spec.rb
@@ -0,0 +1,30 @@
+require './lib/account.rb'
+require 'date'
+
+describe Account do
+
+  let(:person) {instance_double('Person', name: 'Mathias')}
+  subject { described_class.new({owner: person})}
+
+it 'is expected to have an owner' do
+  expect(subject.owner).to eq person
+end
+
+it 'is expected to raise error if no owner is set' do
+  expect { described_class.new}.to raise_error 'An account owner is required'
+end
+
+it 'has expiry date set on initialize' do
+  expected_output = Date.today.next_year(5).strftime('%m/%y')
+  expect(subject.exp_date).to eq expected_output
+end
+
+it 'is expected to have :active status on initialize' do
+  expect(subject.status).to eq :active
+end
+
+it 'deactivates account using instance method' do
+  subject.deactivate
+  expect(subject.status).to eq :deactivated
+end
+end
